8ad583e73ecada4e46773eec3d7a61cfec3d45ec
Gourav Gupta <gouravgupta7799@gmail.com>
2024-01-20 12:29:19 +0530

    auto logout

--- src/components/Store/Auth-context.js
+++ src/components/Store/Auth-context.js
@@ -10,15 +10,21 @@ export const AuthContextProvider = (props) => {
   const intitalToken = localStorage.getItem('token')
   const [token, setToken] = useState(intitalToken)
   const UserIsLoggedIn = !!token
+  let timeOut;
 
   const loginHandler = (token) => {
     setToken(token);
     localStorage.setItem('token', token)
+    timeOut = setTimeout(() => {
+      localStorage.removeItem('token')
+      console.log('setTimeout')
+    }, (5 * 60 * 1000))
 
   }
   const logoutHandler = () => {
     setToken(null)
     localStorage.removeItem('token')
+    clearTimeout(timeOut)
   }
   const contextValue = {
     token: token,
